5cc7d6fe7f10177693df21d2d51fd5651d6e3df0
Achmad <achmad@mail.com>
2023-02-25 23:15:27 +0700

    menggantikan app.blade.php ecommerce menjadi admin warung mbo'e

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ecommerce</title>
+    <title>Admin Warung Mbo'e</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
