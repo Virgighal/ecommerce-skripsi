fa8915cc66195dda722dd84322e4dc0fc6ace283
Achmad <achmad@mail.com>
2023-07-27 01:15:08 +0700

    fix

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12 col-md-6">
-                <h1>Report</h1>
+                <h1>Reports</h1>
                 </div>
                 <div class="col-sm-12 col-md-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
-                    <li class="breadcrumb-item active">Report</li>
+                    <li class="breadcrumb-item active">Reports</li>
                 </ol>
                 </div>
             </div>
